6c4ceddeef5bfa213ddf8386609a213feb793414
thepro57 <Amrithavk777@gmail.com>
2025-09-01 14:46:22 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -1,1 +1,12 @@
+package com.example.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
 
